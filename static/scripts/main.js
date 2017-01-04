1a4b25f8d8ae345b4a60c978b6eb088b60d76f86
LingWind <shengyichen@Shengyis-MacBook-Pro.local>
2017-01-03 22:19:29 -0500

    Updated HTML

---
+++ static/scripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$(".col-md-4").on("click", function(){
+		alert("This feature is still under development, please come back later");
+	})
+
+	$(".btn").on("click", function(){
+		alert("This feature is still under development, please come back later");
+	})
+});
