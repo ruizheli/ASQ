38f7eec395977aed553b664c41836fd050f74fe8
n0whereRuoxi <n0where@Ruoxis-MacBook-Pro.local>
2017-01-05 19:24:45 -0500

    Added transcript

--- maintest.py
+++ maintest.py
@@ -10,31 +10,25 @@ file_dir = "./transcripts/DFA_01.flac"
 command = "python ./autosub/autosub.py %s -F json" %(file_dir)
 subprocess.call(command, shell=True)	
 
+import json
+file_dir = './transcripts/DFA_01.json'
+json_data = open(file_dir)
+d = json.load(json_data)
+s = ''
+for i in d:
+	s = s + ' ' + i['content']
+
 # Whoosh the search engine
 schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
 if not os.path.exists("index"):
     os.mkdir("index")
 ix = create_in("index", schema)
 writer = ix.writer()
-writer.add_document(title=u"First document", path=u"/a", content=u"This is the first document we've added!")
-writer.add_document(title=u"Second document", path=u"/b", content=u"The second one is even more interesting!")
-f = codecs.open("/Users/n0where/Desktop/DFA.srt","r","utf-8").read()
-writer.add_document(title=u"DFA", path=u"/Users/n0where/Desktop/DFA.srt", content=f)
-#writer.add_document(title=u"Third document", path=u"/b", content=u"The second one is even more interesting!The second one is even more interesting!The second one is even more interesting!The second one is even more interesting!The second one is even more interesting!The second one is even more interesting!The second one is even more interesting!The second one is even more interesting!The second one is even more interesting!The second one is even more interesting! star")
-f = codecs.open("/Users/n0where/Desktop/dictionary.txt","r","utf-8").read()
-writer.add_document(title=u"Dic", path=u"/Users/n0where/Desktop/Dic.srt", content=f)
-writer.add_document(title=u"Dic2", path=u"/Users/n0where/Desktop/Dic.srt", content=f)
-writer.add_document(title=u"Dic3", path=u"/Users/n0where/Desktop/Dic.srt", content=f)
-writer.add_document(title=u"Dic4", path=u"/Users/n0where/Desktop/Dic.srt", content=f)
-writer.add_document(title=u"Dic5", path=u"/Users/n0where/Desktop/Dic.srt", content=f)
-writer.add_document(title=u"Dic6", path=u"/Users/n0where/Desktop/Dic.srt", content=f)
-writer.add_document(title=u"Dic7", path=u"/Users/n0where/Desktop/Dic.srt", content=f)
-writer.add_document(title=u"Dic8", path=u"/Users/n0where/Desktop/Dic.srt", content=f)
-writer.add_document(title=u"Dic9", path=u"/Users/n0where/Desktop/Dic.srt", content=f)
+writer.add_document(title=u"DFA", path=u"./transcripts/DFA_01.flac", content=s)
 writer.commit()
 print("Written")
 with ix.searcher() as searcher:
-	query = QueryParser("content", ix.schema).parse("aaah")
+	query = QueryParser("content", ix.schema).parse("video")
 	results = searcher.search(query)
 	print(results[0])
 
--- testjson.py
+++
@@ -1,8 +0,0 @@
-import json
-file_dir = './transcripts/DFA_01.json'
-with open(file_dir) as json_data:
-    d = json.load(json_data)
-    s = ''
-    for i in d:
-    	s = s + ' ' + i['content']
-    print s
