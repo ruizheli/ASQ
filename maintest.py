6446b52de93fa652552302730612b647e177c643
n0whereRuoxi <rli12314@terpmail.umd.edu>
2017-01-07 06:31:59 -0500

    Add files via upload

--- maintest.py
+++ maintest.py
@@ -5,33 +5,66 @@ from whoosh.fields import *
 import os
 import codecs
 from whoosh.qparser import QueryParser
-
-file_dir = "./transcripts/DFA_01.flac"
-command = "python ./autosub/autosub.py %s -F json" %(file_dir)
-subprocess.call(command, shell=True)	
-
+import whoosh.index as index
 import json
-file_dir = './transcripts/DFA_01.json'
-json_data = open(file_dir)
-d = json.load(json_data)
-s = ''
-for i in d:
-	s = s + ' ' + i['content']
+
+vedio_formats = ['mp4','avi','wmv','mov'] # 1
+audio_formats = ['wav','flac','mp3','aiff'] # 2
+
+def file_upload(file_name_and_type,file_dir = "./transcripts/"):
+	regex = r"(.+)\.(.+)"
+	if re.search(regex, file_name_and_type):
+	    match = re.search(regex, file_name_and_type)
+	    file_name = match.group(1)
+	    file_type = match.group(2).lower()
+	else: 
+		raise fileNameError('fileNameError')
+	file_pwd = file_dir + file_name_and_type
+	transcripts_timed_pwd = file_dir + file_name + '.json'
+	autosubing(file_pwd,transcripts_timed_pwd,file_type)
+	whoosh_indexing(file_name,file_pwd,transcripts_timed_pwd)
+
+def autosubing(file_pwd,transcripts_timed_pwd,file_type):
+	if not os.path.isfile(transcripts_timed_pwd):
+		if file_format(file_type) == 1:	
+			command = "python ./autosub/autosub.py -F json -V %s" %(file_pwd)
+		else: 
+			command = "python ./autosub/autosub.py %s -F json" %(file_pwd)
+		subprocess.call(command, shell=True)	
+		print "Autosubed"
+	else: 
+		print 'file has already been autosubed'
+
+def whoosh_indexing(file_name,file_pwd,transcripts_timed_pwd):
+	json_data = open(transcripts_timed_pwd)
+	transcripts_timed = json.load(json_data)
+	transcripts_content = ''
+	for i in transcripts_timed:
+		transcripts_content = transcripts_content + ' ' + i['content']
+	# Whoosh the search engine
+	schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
+	if not os.path.exists("index"):
+	    os.mkdir("index")
+	ix = index.open_dir("index")
+	writer = ix.writer()
+	writer.update_document(title=file_name.decode('utf-8'), path=file_pwd.decode('utf-8'), content=transcripts_content.decode('utf-8'))
+	writer.commit()
+	print("Written")
+	with ix.searcher() as searcher:
+		query = QueryParser("content", ix.schema).parse("atom")
+		results = searcher.search(query)
+		for i in results:
+			print(i)
+	json_data.close()
+
+# throw formatError
+def file_format(file_type):
+	if file_type in vedio_formats:
+		return 1;
+	elif file_type in audio_formats:
+		return 2
+	else: raise Exception('Format prohibited')
+
+file_upload('DFA_01.flac')
 
 
-# Whoosh the search engine
-schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
-if not os.path.exists("index"):
-    os.mkdir("index")
-ix = create_in("index", schema)
-writer = ix.writer()
-writer.add_document(title=u"DFA", path=u"./transcripts/DFA_01.flac", content=s)
-writer.commit()
-print("Written")
-with ix.searcher() as searcher:
-	query = QueryParser("content", ix.schema).parse("video")
-	results = searcher.search(query)
-	print(results[0])
-
-
-json_data.close()
