8e244678116203625005e1008215565d76bbe728
RuizheLi <ruizheli97@gmail.com>
2017-01-08 18:50:25 -0500

    Testing

--- upload.py
+++ upload.py
@@ -34,15 +34,15 @@ def upload_data():
 
 
 
-	title = request.forms.get('title')
-	author = request.forms.get('author')
+	# title = request.forms.get('title')
+	# author = request.forms.get('author')
 
-	conn = pymssql.connect(server='asq-bottle.database.windows.net',user='ruizheli@asq-bottle.database.windows.net', password='Fzj990418.', database='asq-bottle')
-	cursor = conn.cursor()
-	#cursor.execute('INSERT INTO dbo.asq_file_data (title, author) VALUES ('Lecture', 'Anwar')')
-	cursor.execute('INSERT INTO [dbo].[asq_file_data] ([title] ,[author],[tags],[description],[subject],[format],[file],[transcript_timed],[key_time_map]) VALUES (N\'' + title + '\',N\'' + author + '\',N\'testTags\',N\'testDescription\',N\'CS\',N\'video\',convert(varbinary, \'testFile\'),convert(varbinary, \'testTranscript\'),convert(varbinary, \'testMap\'))')
+	# conn = pymssql.connect(server='asq-bottle.database.windows.net',user='ruizheli@asq-bottle.database.windows.net', password='Fzj990418.', database='asq-bottle')
+	# cursor = conn.cursor()
+	# #cursor.execute('INSERT INTO dbo.asq_file_data (title, author) VALUES ('Lecture', 'Anwar')')
+	# cursor.execute('INSERT INTO [dbo].[asq_file_data] ([title] ,[author],[tags],[description],[subject],[format],[file],[transcript_timed],[key_time_map]) VALUES (N\'' + title + '\',N\'' + author + '\',N\'testTags\',N\'testDescription\',N\'CS\',N\'video\',convert(varbinary, \'testFile\'),convert(varbinary, \'testTranscript\'),convert(varbinary, \'testMap\'))')
 
-	conn.commit()
-	conn.close()
+	# conn.commit()
+	# conn.close()
 
 	redirect('/upload/upload_success')
