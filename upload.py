bb508649d88ced1d153803a9ed5d2a576d0c882e
RuizheLi <ruizheli97@gmail.com>
2017-01-09 05:36:39 -0500

    Test pymssql

--- upload.py
+++ upload.py
@@ -1,6 +1,6 @@
 from bottle import route, run, template, view, redirect, post, request
-import pyodbc
-# import pymssql
+# import pyodbc
+import pymssql
 
 # @route('/upload/upload_data')
 # def upload_data():
@@ -16,33 +16,33 @@ import pyodbc
 
 @route('/upload/upload_data', method='POST')
 def upload_data():
-	print("connecting")
-	server = 'tcp:asq-bottle.database.windows.net'
-	database = 'asq-bottle'
-	username = 'ruizheli@asq-bottle'
-	password = 'Fzj990418.'
-	#for mac
-	# driver = '{/usr/local/lib/libtdsodbc.so}'
-	driver= '{ODBC Driver 13 for SQL Server}'
-	cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
-	print("connected")
-	cursor = cnxn.cursor()
-
-	title = request.forms.get('title')
-	author = request.forms.get('author')
-	cursor.execute('INSERT INTO [dbo].[asq_file_data] ([title] ,[author],[tags],[description],[subject],[format],[file],[transcript_timed],[key_time_map]) VALUES (N\'' + title + '\',N\'' + author + '\',N\'testTags\',N\'testDescription\',N\'CS\',N\'video\',convert(varbinary, \'testFile\'),convert(varbinary, \'testTranscript\'),convert(varbinary, \'testMap\'))')
-
-
+	# print("connecting")
+	# server = 'tcp:asq-bottle.database.windows.net'
+	# database = 'asq-bottle'
+	# username = 'ruizheli@asq-bottle'
+	# password = 'Fzj990418.'
+	# #for mac
+	# # driver = '{/usr/local/lib/libtdsodbc.so}'
+	# driver= '{ODBC Driver 13 for SQL Server}'
+	# cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+	# print("connected")
+	# cursor = cnxn.cursor()
 
 	# title = request.forms.get('title')
 	# author = request.forms.get('author')
-
-	# conn = pymssql.connect(server='asq-bottle.database.windows.net',user='ruizheli@asq-bottle.database.windows.net', password='Fzj990418.', database='asq-bottle')
-	# cursor = conn.cursor()
-	# #cursor.execute('INSERT INTO dbo.asq_file_data (title, author) VALUES ('Lecture', 'Anwar')')
 	# cursor.execute('INSERT INTO [dbo].[asq_file_data] ([title] ,[author],[tags],[description],[subject],[format],[file],[transcript_timed],[key_time_map]) VALUES (N\'' + title + '\',N\'' + author + '\',N\'testTags\',N\'testDescription\',N\'CS\',N\'video\',convert(varbinary, \'testFile\'),convert(varbinary, \'testTranscript\'),convert(varbinary, \'testMap\'))')
 
-	# conn.commit()
-	# conn.close()
+
+
+	title = request.forms.get('title')
+	author = request.forms.get('author')
+
+	conn = pymssql.connect(server='asq-bottle.database.windows.net',user='ruizheli@asq-bottle.database.windows.net', password='Fzj990418.', database='asq-bottle')
+	cursor = conn.cursor()
+	#cursor.execute('INSERT INTO dbo.asq_file_data (title, author) VALUES ('Lecture', 'Anwar')')
+	cursor.execute('INSERT INTO [dbo].[asq_file_data] ([title] ,[author],[tags],[description],[subject],[format],[file],[transcript_timed],[key_time_map]) VALUES (N\'' + title + '\',N\'' + author + '\',N\'testTags\',N\'testDescription\',N\'CS\',N\'video\',convert(varbinary, \'testFile\'),convert(varbinary, \'testTranscript\'),convert(varbinary, \'testMap\'))')
+
+	conn.commit()
+	conn.close()
 
 	redirect('/upload/upload_success')
