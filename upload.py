6d4689094b6a23da05cc51a773e49172f85a4b6f
RuizheLi <ruizheli97@gmail.com>
2017-01-10 22:10:09 -0500

    Switching to MongoDB

--- upload.py
+++ upload.py
@@ -10,50 +10,6 @@ from pprint import pprint
 
 @route('/upload/upload_data', method='POST')
 def upload_data():
-	# server = 'tcp:asq-bottle.database.windows.net'
-	# database = 'asq-bottle'
-	# username = 'ruizheli@asq-bottle'
-	# password = 'Fzj990418.'
-	# driver= '{ODBC Driver 13 for SQL Server}'
-
-	# # pyodbc part, for deploying only
-	# # conn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
-	
-	# # pymssql part, for testing only
-	# conn = pymssql.connect(server='asq-bottle.database.windows.net',user='ruizheli@asq-bottle.database.windows.net', password='Fzj990418.', database='asq-bottle')
-
-	# # logics for uploading
-	# cursor = conn.cursor()
-
-	# title = request.forms.get('title')
-	# author = request.forms.get('author')
-	# tags = request.forms.get('tags')
-	# description = request.forms.get('description')
-	# category = request.forms.get('category')
-	# file = request.files.get('file')
-
-	# print('Encoding as base64')
-	# file_content = base64.b64encode(file.file.read())
-	# print('Encoding finished')
-	# # print(file_content)
-
-	# query = """INSERT INTO [dbo].[asq_file_data] ([title], [author], [tags], [description], [subject], [format], [file], [transcript_timed], [key_time_map]) VALUES (N\'%s\', N\'%s\', N\'%s\',N\'%s\', N\'%s\', N\'video\', convert(binary, \'%s\'), convert(varbinary, \'testTranscript\'), convert(varbinary, \'testMap\'))"""
-
-	# cursor.execute(query % (title, author, tags, description, category, file_content,))
-
-	# query = """SELECT [file] FROM [dbo].[asq_file_data] """
-
- #  	cursor.execute(query)
-
- #  	for (file) in cursor:
- #  		print(file)
-
-	# conn.commit()
-	# conn.close()
-
-	# ===================================
-	# Testing with MongoDB
-
 	title = request.forms.get('title')
 	author = request.forms.get('author')
 	tags = request.forms.get('tags')
@@ -85,5 +41,5 @@ def upload_data():
 	post_id = asq_file_data.insert_one(post).inserted_id
 
 	print(post_id)
-	
+
 	redirect('/upload/upload_success')
